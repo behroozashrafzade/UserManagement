63685e44caa7cf975348d5891bb32b21dcf4c623
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-03 18:38:15 +0330

    update class AppDbConext insert  dbset table user

---
+++ WebApplication.9.0/AppDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication._9._0.Entities;
+
+namespace WebApplication._9._0;
+
+public class AppDbContext(DbContextOptions options): DbContext(options)
+{
+    public DbSet<UserEntity> Users { get; set; }
+}
---
+++ WebApplication.9.0/Entities/UserEntity.cs
@@ -0,0 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication._9._0.Entities;
+[Table("Users")]
+public class UserEntity
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [MinLength(2)]
+    [MaxLength(50)]
+    public required string Fullname { get; set; }
+    [EmailAddress]
+     public required string Email { get; set; }
+     
+    [Required]
+    [MinLength(10)]
+    [MaxLength(12)]
+    public required string PhoneNumber { get; set; }
+    
+    public DateTime? Birthdate { get; set; }
+
+    public bool IsMarried { get; set; } = false;
+    
+
+}
