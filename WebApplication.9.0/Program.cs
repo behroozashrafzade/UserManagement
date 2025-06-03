f4c300f8157b78ef3c3dcbb3aaa6eac97e54d73b
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-03 18:50:20 +0330

    fix Eroor refrene CreateBuilder & Run

--- WebApplication.9.0/Program.cs
+++ WebApplication.9.0/Program.cs
@@ -1,4 +1,4 @@
-var builder = WebApplication.CreateBuilder(args);
+var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddEndpointsApiExplorer();
--- WebApplication.9.0/obj/Debug/net9.0/WebApplication.9.0.AssemblyInfo.cs
+++ WebApplication.9.0/obj/Debug/net9.0/WebApplication.9.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication.9.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aa00ddc3d87266f2aa99cdc18f0204f58399c00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63685e44caa7cf975348d5891bb32b21dcf4c623")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication.9.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication.9.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
