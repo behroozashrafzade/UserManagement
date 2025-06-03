65969d587a2302c4b096c4a72d5ee7a9b6d4de47
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-03 19:07:46 +0330

    update file program.cs insert config DbConnection

--- WebApplication.9.0/Program.cs
+++ WebApplication.9.0/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication._9._0;
+
 var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped<AppDbContext>();
+builder.Services.AddDbContextPool<AppDbContext>(o =>
+{
+o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 var app = builder.Build();
 
 
@@ -17,5 +24,6 @@ if (app.Environment.IsDevelopment())
 app.MapGet("/hello", () => "Hello World!");
 app.MapGet("/Hi", (string name) => "Hello "+name+"!");
 app.MapPost("Welcome",(string name) =>"Welcome "+name+"!" );
+
 app.Run();
 
