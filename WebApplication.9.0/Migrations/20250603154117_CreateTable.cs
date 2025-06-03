a9a01f156a81023a0ddf7182e40a4d1355cd2c45
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-03 19:26:53 +0330

    ابجاد فابل Redme

---
+++ WebApplication.9.0/Migrations/20250603154117_CreateTable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication._9._0.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Fullname = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "character varying(12)", maxLength: 12, nullable: false),
+                    Birthdate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    IsMarried = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
