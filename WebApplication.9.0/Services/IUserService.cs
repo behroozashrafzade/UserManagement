70a7294c933f71caaba2a58df11ac128ca685456
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-05 18:22:32 +0330

    modified IUserService add dependency injection  and modifie methode Create

---
+++ WebApplication.9.0/Services/IUserService.cs
@@ -0,0 +1,20 @@
+using WebApplication._9._0.Entities;
+
+namespace WebApplication._9._0.Services;
+
+public interface IUserService
+{
+    UserEntity Create(UserEntity user);
+}
+
+public class UserService(AppDbContext dbContext) : IUserService
+{
+   
+    public UserEntity Create(UserEntity user)
+    {
+        //dbContext.Set<UserEntity>().Add(user);
+        var entity=dbContext.Users.Add(user);
+        return entity.Entity;
+    }
+}
+
