72518b71f85ed91041e26928bc62b173d3ed8c18
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-05 18:50:53 +0330

    update file iUserservice  modified calss careate

--- WebApplication.9.0/Services/IUserService.cs
+++ WebApplication.9.0/Services/IUserService.cs
@@ -1,21 +1,50 @@
+using WebApplication._9._0.Dtos;
 using WebApplication._9._0.Entities;
 
 namespace WebApplication._9._0.Services;
 
 public interface IUserService
 {
-    UserEntity Create(UserEntity user);
+    UserResponse Create(UserCreateParams user);
 }
 
 public class UserService(AppDbContext dbContext) : IUserService
 {
    
-    public UserEntity Create(UserEntity user)
+    public UserResponse Create(UserCreateParams dto)
     {
+        UserEntity user=new ()
+        {
+            Id = Guid.CreateVersion7(),
+            Fullname = dto.Fullname,
+            Email = dto.Email,
+            PhoneNumber = dto.PhoneNumber,
+            Birthdate = dto.Birthdate,
+            IsMarried = dto.IsMarried,
+            
+        };
+        
         //dbContext.Set<UserEntity>().Add(user);
         var entity=dbContext.Users.Add(user);
         dbContext.SaveChanges();
-        return entity.Entity;
+
+        int? age = null;
+        if (entity.Entity.Birthdate!=null)
+        {
+            age=DateTime.UtcNow.Year - entity.Entity.Birthdate.Value.Year;
+        }
+        
+        
+        return new UserResponse()
+        {
+            Fullname = entity.Entity.Fullname,
+            Email = entity.Entity.Email,
+            PhoneNumber = entity.Entity.PhoneNumber,
+            Birthdate = entity.Entity.Birthdate,
+            IsMarried = entity.Entity.IsMarried,
+            Age = age
+            
+        };
     }
 }
 
