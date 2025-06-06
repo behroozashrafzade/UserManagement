868b17899cd5d6c167037e9c1fb7c747a11cca6e
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-06 18:38:05 +0330

    update userDto add userupdateparams

--- WebApplication.9.0/Dtos/UserDto.cs
+++ WebApplication.9.0/Dtos/UserDto.cs
@@ -28,6 +28,23 @@ public class UserResponse
     public required string Email { get; set; }
     public required string PhoneNumber { get; set; }
     public DateTime? Birthdate { get; set; }
-    public bool IsMarried { get; set; } = false;
+    public bool IsMarried { get; set; } 
     public int? Age { get; set; }
 }
+public class UserUpdateParams
+{
+    [Required]
+    public required Guid Id { get; set; }
+    
+    public  string? Fullname { get; set; }
+    
+    public  string? Email { get; set; }
+     
+   
+    public  string? PhoneNumber { get; set; }
+    
+    public DateTime? Birthdate { get; set; }
+
+    public bool? IsMarried { get; set; } 
+}
+
