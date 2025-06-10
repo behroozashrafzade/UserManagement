f45295529f26c084c3a85484448d7666b03e6c37
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-10 17:42:15 +0330

    update database addSchool

--- WebApplication.9.0/obj/Debug/net9.0/WebApplication.9.0.AssemblyInfo.cs
+++ WebApplication.9.0/obj/Debug/net9.0/WebApplication.9.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication.9.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0820f85cc6185eacd8aa585656e99c5276999bff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e73e8a5c7b0cc02a415701dd6a655947db9379")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication.9.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication.9.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
