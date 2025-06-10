5a7619ce20f7b8b0c34d9e0352aca47b171413b0
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-10 22:49:30 +0330

    add full class schoolService-3

--- WebApplication.9.0/Program.cs
+++ WebApplication.9.0/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddDbContextPool<AppDbContext>(o =>
 o.UseNpgsql(builder.Configuration.GetConnectionString("DefaultDatabase"));
 });
 builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IClassService, ClassService>();
+builder.Services.AddScoped<ISchoolService, SchoolService>();
 var app = builder.Build();
 
 
@@ -33,18 +35,34 @@ app.MapPost("user/Create",async (IUserService userService,UserCreateParams dto)=
     UserResponse result = await userService.Create(dto);
     return Results.Ok(result);
 });
+app.MapPost("schoo/Create",async (ISchoolService Service,UserCreateParams dto)=>{
+    SchoolEntitiy result = await Service.Create(dto);
+    return Results.Ok(result);
+});
 app.MapGet("user/Read",async (IUserService userService)=>{
     IEnumerable<UserResponse> result = await userService.Read();
+    return Results.Ok(result);
+});
+app.MapGet("user/Read",async (ISchoolService Service)=>{
+    IEnumerable<SchoolEntitiy> result = await Service.Read();
     return Results.Ok(result);
 });
 
 app.MapGet("user/Read{id:guid}",async (IUserService userService,Guid id)=>{
     UserResponse? result = await userService.ReadById(id);
    return result==null ? Results.NotFound() : Results.Ok(result);
+   
+});app.MapGet("School/Read{id:guid}",async (ISchoolService Service,Guid id)=>{
+    SchoolEntitiy? result = await Service.ReadById(id);
+   return result==null ? Results.NotFound() : Results.Ok(result);
 });
 
 app.MapPut("user/Update",async (IUserService userService,UserUpdateParams param)=>{
     UserResponse? result = await userService.Update(param);
+    return result==null ? Results.NotFound() : Results.Ok(result);
+    
+});app.MapPut("School/Update",async (ISchoolService Service,UserUpdateParams param)=>{
+    SchoolEntitiy? result = await Service.Update(param);
     return result==null ? Results.NotFound() : Results.Ok(result);
 });
 
@@ -52,7 +70,10 @@ app.MapDelete("user/Delete",async (IUserService userService,Guid id)=>{
     await userService.Delete(id);
     return  Results.Ok();
 });
-
+app.MapDelete("School/Delete",async (ISchoolService Service,Guid id)=>{
+    await Service.Delete(id);
+    return  Results.Ok();
+});
 
 app.Run();
 
