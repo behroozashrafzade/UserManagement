bd03112dd6d3e7b4e06c6754107a84004a99c89a
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-10 18:05:18 +0330

    add dbset in AppDbContext.cs

--- WebApplication.9.0/AppDbContext.cs
+++ WebApplication.9.0/AppDbContext.cs
@@ -6,4 +6,7 @@ namespace WebApplication._9._0;
 public class AppDbContext(DbContextOptions options): DbContext(options)
 {
     public DbSet<UserEntity> Users { get; set; }
+    public DbSet<SchoolEntitiy> School { get; set; }
+    public DbSet<ClassEntity> Class { get; set; }
+    
 }
--- WebApplication.9.0/obj/Debug/net9.0/WebApplication.9.0.AssemblyInfo.cs
+++ WebApplication.9.0/obj/Debug/net9.0/WebApplication.9.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication.9.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4e73e8a5c7b0cc02a415701dd6a655947db9379")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f45295529f26c084c3a85484448d7666b03e6c37")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication.9.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication.9.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
