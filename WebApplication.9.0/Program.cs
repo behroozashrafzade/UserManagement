feb63f848334b04bc94fec86388853a5e0485bed
behroozashrafzade <behrooz.ashrafzade@gmail.com>
2025-06-12 11:14:43 +0330

    update file program.cs add map all for school and class

--- WebApplication.9.0/Program.cs
+++ WebApplication.9.0/Program.cs
@@ -31,49 +31,113 @@ app.MapGet("/hello", () => "Hello World!");
 app.MapGet("/Hi", (string name) => "Hello "+name+"!");
 app.MapPost("Welcome",(string name) =>"Welcome "+name+"!" );
 
+#region User
+
+//     ---> Create
 app.MapPost("user/Create",async (IUserService userService,UserCreateParams dto)=>{
     UserResponse result = await userService.Create(dto);
     return Results.Ok(result);
 });
-app.MapPost("schoo/Create",async (ISchoolService Service,UserCreateParams dto)=>{
-    SchoolEntitiy result = await Service.Create(dto);
-    return Results.Ok(result);
-});
+
+//     ---> Read
 app.MapGet("user/Read",async (IUserService userService)=>{
     IEnumerable<UserResponse> result = await userService.Read();
     return Results.Ok(result);
 });
-app.MapGet("user/Read",async (ISchoolService Service)=>{
-    IEnumerable<SchoolEntitiy> result = await Service.Read();
-    return Results.Ok(result);
-});
 
+//     ---> Read By Id
 app.MapGet("user/Read{id:guid}",async (IUserService userService,Guid id)=>{
     UserResponse? result = await userService.ReadById(id);
-   return result==null ? Results.NotFound() : Results.Ok(result);
-   
-});app.MapGet("School/Read{id:guid}",async (ISchoolService Service,Guid id)=>{
-    SchoolEntitiy? result = await Service.ReadById(id);
-   return result==null ? Results.NotFound() : Results.Ok(result);
+    return result==null ? Results.NotFound() : Results.Ok(result);
 });
-
+//     ---> Delete
+app.MapDelete("user/Delete",async (IUserService userService,Guid id)=>{
+    await userService.Delete(id);
+    return  Results.Ok(); 
+       
+});
+//     ---> Update
 app.MapPut("user/Update",async (IUserService userService,UserUpdateParams param)=>{
     UserResponse? result = await userService.Update(param);
     return result==null ? Results.NotFound() : Results.Ok(result);
-    
-});app.MapPut("School/Update",async (ISchoolService Service,UserUpdateParams param)=>{
+   
+});
+
+#endregion
+
+
+
+#region class
+//     ---> Create
+
+app.MapPost("Class/Create",async (IClassService Service,ClassEntity dto)=>{
+    ClassEntity result = await Service.Create(dto);
+    return Results.Ok(result);
+});
+//     ---> Read
+app.MapGet("Class/Read",async (IClassService Service)=>{
+   IEnumerable<ClassEntity> result = await Service.Read();
+    return Results.Ok(result);
+});
+//     ---> Read By Id
+app.MapGet("Class/Read{id:guid}",async (IClassService Service,Guid id)=>{
+    ClassEntity? result = await Service.ReadById(id);
+    return result==null? Results.NotFound() : Results.Ok(result);
+});
+//     ---> Delete
+app.MapDelete("Class/Delete",async (IClassService Service,Guid id)=>{
+    await Service.Delete(id);
+    return  Results.Ok(); 
+       
+});
+//     ---> Update
+app.MapPut("Class/Update",async (IClassService Service,ClassEntity param)=>{
+    ClassEntity? result = await Service.Update(param);
+    return result==null ? Results.NotFound() : Results.Ok(result);
+   
+});
+#endregion
+
+#region School
+//     ---> Read 
+app.MapGet("School/Read{id:guid}",async (ISchoolService Service,Guid id)=>{
+    SchoolEntitiy? result = await Service.ReadById(id);
+   return result==null ? Results.NotFound() : Results.Ok(result);
+});
+       
+//     ---> Update 
+app.MapPut("School/Update",async (ISchoolService Service,SchoolEntitiy param)=>{
     SchoolEntitiy? result = await Service.Update(param);
     return result==null ? Results.NotFound() : Results.Ok(result);
 });
 
-app.MapDelete("user/Delete",async (IUserService userService,Guid id)=>{
-    await userService.Delete(id);
-    return  Results.Ok();
+//     ---> Create 
+app.MapPost("school/Create",async (ISchoolService Service,SchoolEntitiy param)=>{
+        SchoolEntitiy result = await Service.Create(param);
+        return Results.Ok(result);
 });
+ 
+//     ---> Delete 
 app.MapDelete("School/Delete",async (ISchoolService Service,Guid id)=>{
     await Service.Delete(id);
     return  Results.Ok();
 });
 
+
+//     ---> Read By Id
+app.MapGet("School/Read{id:guid}",async (ISchoolService Service,Guid id)=>{
+    SchoolEntitiy? result = await Service.ReadById(id);
+    return result==null ? Results.NotFound() : Results.Ok(result);
+});
+#endregion
+
+
 app.Run();
+
+
+
+
+
+
+
 
